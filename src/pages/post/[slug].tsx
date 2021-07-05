f9faad99dc428b04ed670b8fafd1eef8a7bc6c7b
henrique dantas <paulo.henrique@sapienstec.com>
2021-07-05 15:40:28 -0300

    finalizando aplicação

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -6,6 +6,12 @@ import Header from '../../components/Header';
 import {AiOutlineCalendar} from 'react-icons/ai';
 import {BsFillPersonFill} from 'react-icons/bs';
 import { BiTimeFive} from 'react-icons/bi';
+import { RichText } from 'prismic-dom';
+import Prismic from '@prismicio/client';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { ptBR } from 'date-fns/locale';
+import { format } from 'date-fns';
 interface Post {
   first_publication_date: string | null;
   data: {
@@ -27,37 +33,59 @@ interface PostProps {
   post: Post;
 }
 
- export default function Post({post}:PostProps) {
+ export default function Post({post}:PostProps): JSX.Element {
+    const totalWords = post.data.content.reduce((total, contentItem) => {
+      total += contentItem.heading.split(' ').length;
+      const words = contentItem.body.map(item => item.text.split(' ').length);
+      words.map( word => (total += word));
+      return total;
+    }, 0 )
+
+
+    const readTime = Math.ceil(totalWords / 200);
+
+    const router = useRouter();
+    if(router.isFallback) {
+      return <h1>Carregando...</h1>
+    }
+
+    const formatedDate = format(
+      new Date(post.first_publication_date), 'dd MMM yyyy', {
+        locale: ptBR
+      }
+    )
+
     return (
       <>
+      <Head>
+        <title>{post.data.title}</title>
+      </Head>
       <Header/>
-      <img src="/Banner.jpg" alt="banner" className={styles.banner} />
+      <img src={post.data.banner.url} alt="banner" className={styles.banner} />
       <main className={commonStyles.container}>
         <div className={styles.post}>
           <div className={styles.header}>
-            <strong>Criando um app CRA do zero</strong>
+            <strong>{post.data.title}</strong>
             <ul>
                 <li>
-                  <AiOutlineCalendar/> data
+                  <AiOutlineCalendar/> {formatedDate}
                 </li>
                 <li>
-                  <BsFillPersonFill/> autor
+                  <BsFillPersonFill/> {post.data.author}
                 </li>
                 <li>
-                  <BiTimeFive/> 4min
+                  <BiTimeFive/> {`${readTime}min`}
                 </li>
             </ul>
           </div>
-          <article>
-            <h1>Título do Post</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-              It has survived not only five centuries, but also the leap into electronic typesetting, 
-              remaining essentially unchanged. It was popularised in the 1960s with the release of 
-              Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-              software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          </article>
+         {post.data.content.map(content => (
+           <article key = {content.heading}>
+             <h2>{content.heading}</h2>
+             <div className={styles.postContent} dangerouslySetInnerHTML={{__html: RichText.asHtml(content.body)}}>
+
+             </div>
+           </article>
+         ))}
         </div>  
       </main>
       </>
@@ -66,19 +94,50 @@ interface PostProps {
 
 
 
-//  export const getStaticPaths:GetStaticPaths = async () => {
-//    const prismic = getPrismicClient();
-//    const posts = await prismic.query();
+  export const getStaticPaths:GetStaticPaths = async () => {
+     const prismic = getPrismicClient();
+     const posts = await prismic.query([Prismic.Predicates.at('document.type', 'posts'),]);
+     const paths = posts.results.map(post => {
+       return {
+         params: {
+           slug: post.uid,
+         }
+       }
+     }) 
 
-    
-//  };
+    return {
+      paths,
+      fallback: true,
+    }
+  };
 
-//  export const getStaticProps:GetStaticProps = async context => {
-//    const prismic = getPrismicClient();
-//    const response = await prismic.getByUID();
+  export const getStaticProps:GetStaticProps = async context => {
+    const prismic = getPrismicClient();
+    const { slug } = context.params;
+    const response = await prismic.getByUID('posts', String(slug), {});
 
-//    return {
-//      props {
-//      }
-//    }
-//  };
+    const post = {
+      uid: response.uid,
+      first_publication_date: response.first_publication_date,
+      data: {
+        title: response.data.title,
+        subtitle: response.data.subtitle,
+        author: response.data.author,
+        banner: {
+          url: response.data.banner.url,
+        },
+        content: response.data.content.map(content => {
+          return {
+            heading: content.heading,
+            body: [...content.body],
+          };
+        }),
+      }
+    }
+
+    return {
+      props: {
+        post,
+      }
+    }
+  };
